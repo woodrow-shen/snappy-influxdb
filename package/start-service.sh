e40b76b116a39d28954f84626cbc48edd61adb33
Woodrow Shen <woodrow.shen@canonical.com>
2015-12-28 17:16:15 +0800

    Add configs for iot-demo

--- package/start-service.sh
+++ package/start-service.sh
@@ -4,12 +4,15 @@ set -e
 set -x
 
 mkdir -p $SNAP_APP_DATA_PATH/etc/influxdb/
+mkdir -p $SNAP_APP_DATA_PATH/var/
 
 cp -n $SNAP_APP_PATH/etc/influxdb/influxdb.conf $SNAP_APP_DATA_PATH/etc/influxdb/influxdb.conf
 SNAP_APP_DATA_PATH_ESCAPE=$(echo $SNAP_APP_DATA_PATH | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')
 sed -i "s/SNAP_APP_DATA_PATH/$SNAP_APP_DATA_PATH_ESCAPE/g" $SNAP_APP_DATA_PATH/etc/influxdb/influxdb.conf
 SNAP_APP_PATH_ESCAPE=$(echo $SNAP_APP_PATH | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')
 sed -i "s/SNAP_APP_PATH/$SNAP_APP_PATH_ESCAPE/g" $SNAP_APP_DATA_PATH/etc/influxdb/influxdb.conf
+
+cp -r $SNAP_APP_PATH/var/* $SNAP_APP_DATA_PATH/var/
 
 $SNAP_APP_PATH/magic-bin/influxd -config=$SNAP_APP_DATA_PATH/etc/influxdb/influxdb.conf
 
